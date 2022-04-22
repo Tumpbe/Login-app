fc7a1a2458cc0e5f9e8cb54b0315cf5d9afa3e07
Sami Mononen <sami.mononen@tuni.fi>
2022-04-22 15:19:08 +0300

    Start backend development

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const db = require('./db');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
